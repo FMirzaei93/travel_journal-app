98404aa3f90f91b22258170221efcdc3cf3fd617
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-18 16:26:41 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import Header from "./Header";
 import ListItem from "./ListItem";
+import data from "../Data";
 
 export default function App() {
+  const listElements = data.map((item) => {
+    return (
+      <ListItem
+        key={item.id}
+        {...item}
+        //
+      />
+    );
+  });
+
   return (
     <div>
       <Header />
-      <ListItem />
+      <div className='items'>{listElements}</div>
     </div>
   );
 }
