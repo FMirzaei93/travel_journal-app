d50c79c785e692a4bb48eb3dd25f17eeb0d2e90e
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-17 17:24:32 -0700

    initial files modifications

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import ListItem from "./ListItem";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <ListItem />
+    </div>
+  );
+}
