4af7f3dcddbb10792f02421dc62dc4782af5adc6
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-18 16:26:47 -0700

    Update ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,3 +1,17 @@
-export default function ListItem() {
-  return <div></div>;
+export default function ListItem(props) {
+  return (
+    <section className='list-item'>
+      <img src={props.imageUrl} alt='place image' />
+      <div className='right-side'>
+        <i className='fas fa-map-marker-alt'></i>
+        <span className='location'> {props.location}</span>
+        <a href={props.googleMapsUrl}>View on Google maps</a>
+        <h1>{props.title}</h1>
+        <div className='date'>
+          <span>{props.startDate}</span> - <span>{props.endDate}</span>
+        </div>
+        <p className='description'>{props.description}</p>
+      </div>
+    </section>
+  );
 }
